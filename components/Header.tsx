5b450ed8bbf1204996268021ef8f237c211c14e3
Simon <ferlat.simon@gmail.com>
2023-08-01 14:25:19 +0200

    implementing reader's improvement suggestion

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,12 +7,12 @@ import LocaleSwitcher from './LocaleSwitcher';
 export default function Header() {
     const t = useTranslations('Header')
     return (
-        <div className="bg-gray-200 shadow flex">
+        <header className="bg-gray-200 shadow flex">
             <nav className="container flex px-2 py-2 gap-5 ">
                 <Link href="/">{t('home')}</Link>
                 <Link href="/about">{t('about')}</Link>
             </nav>
             <LocaleSwitcher />
-        </div>
+        </header>
     )
 }
